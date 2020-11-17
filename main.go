2dd4fcbfa082efd9eeed7805b562344171b3a123
Vivek Singh <vivek03.singh@icloud.com>
2020-11-17 08:17:06 +0530

    correction in main method

--- main.go
+++ main.go
@@ -1,19 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	c := make(chan int) // unbuffered channel of integers
-	go func() {
-		sendMessage("Hi")
-		c <- 1
-	}()
-	sendMessage("Vivek")
-	<-c
-}
 
-func sendMessage(message string) {
-	fmt.Println(message)
 }
